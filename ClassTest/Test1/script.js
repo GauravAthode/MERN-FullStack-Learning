6a148edb91c564406966b74d7dc0e3a4e4675f66
GauravAthode <gauravathode123@gmail.com>
2025-12-08 20:27:29 +0530

    Add HTML and JavaScript for State Map Interaction and Password Manager
    - Created index.html and script.js for a state map application that allows users to search for Indian states and display their capitals on a map.
    - Implemented a search function that places a flag icon on the map based on the user's input.
    - Added index.html and script.js for a Password Manager application that allows users to store website credentials and download them as a CSV file.
    - Implemented functions to add data to local storage and download the stored data.

--- ClassTest/Test1/script.js
+++ ClassTest/Test1/script.js
@@ -1,8 +1,11 @@
 function Enroll() {
     alert("Thank you for Enrolling!");
+    
 
 }
 function Submit(){
     alert("Thank you for contacting us!");
+    const name = document.getElementById("name").value;
+    console.log("Name: " + name);
 
 }
