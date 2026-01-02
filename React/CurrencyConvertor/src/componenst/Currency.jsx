e8e9ec550b62d43db3a2a68d7b57e4f278295f49
GauravAthode <gauravathode123@gmail.com>
2026-01-02 19:10:51 +0530

    commit

--- React/CurrencyConvertor/src/componenst/Currency.jsx
+++ React/CurrencyConvertor/src/componenst/Currency.jsx
@@ -1,19 +1,41 @@
 import React, { useState } from "react";
 import CountryData from "../assets/CountryData.json";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 const Currency = () => {
   const [from, setFrom] = useState("");
   const [to, setTo] = useState("");
-  console.log(CountryData);
+  const [fromAmt, setFromAmt] = useState("");
+  const [toAmt, setToAmt] = useState("");
+
+  const Convert = async () => {
+    if (!from || !to || !fromAmt) {
+      toast.error("Some Fields Missing");
+      return;
+    }
+    try {
+      const res = await axios.get(
+        `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${from
+          .split(" ")[0]
+          .toLowerCase()}.json`
+      );
+
+      
+      setToAmt(fromAmt*res.data[from.split(" ")[0].toLowerCase()][to.split(" ")[0].toLowerCase()]);
+      
+    } catch (error) {}
+  };
+
   return (
     <>
       <div className="bg-amber-50 h-screen p-5">
-        <div className="w-3xl bg-white rounded shadow border p-3 mx-auto">
-          <div className="grid grid-cols-2 gap-5">
+        <div className="w-3xl bg-white rounded shadow border p-3 mx-auto space-y-5">
+          <div className="grid grid-cols-2 gap-10">
             <div className="flex gap-3 border rounded px-3">
               {from && (
                 <img
-                  src={`https://flagsapi.com/${from.split("")[1]}/flat/64.png`}
+                  src={`https://flagsapi.com/${from.split(" ")[1]}/flat/48.png`}
                   alt=""
                 />
               )}
@@ -21,12 +43,12 @@ const Currency = () => {
                 name="from"
                 value={from}
                 onChange={(e) => setFrom(e.target.value)}
-                className="border p-3  rounded  overflow-hidden w-full  focus:outline-none"
+                className=" p-3 overflow-hidden w-full focus:outline-none"
               >
-                <option>--Select Country--</option>
+                <option value="">-Select Country-</option>
                 {CountryData.map((country, idx) => (
                   <option
-                    value={country.currencyCode + " " + country.countryCode}
+                    value={country.CurrencyCode + " " + country.CountryCode}
                     key={idx}
                   >
                     {country.CountryName}
@@ -35,17 +57,23 @@ const Currency = () => {
               </select>
             </div>
 
-            <div>
+            <div className="flex gap-3 border rounded px-3">
+              {to && (
+                <img
+                  src={`https://flagsapi.com/${to.split(" ")[1]}/flat/48.png`}
+                  alt=""
+                />
+              )}
               <select
                 name="to"
                 value={to}
                 onChange={(e) => setTo(e.target.value)}
-                className="border p-3  rounded  overflow-hidden"
+                className=" p-3 overflow-hidden w-full focus:outline-none"
               >
-                <option>--Select Country--</option>
+                <option value="">-Select Country-</option>
                 {CountryData.map((country, idx) => (
                   <option
-                    value={country.currencyCode + " " + country.countryCode}
+                    value={country.CurrencyCode + " " + country.CountryCode}
                     key={idx}
                   >
                     {country.CountryName}
@@ -53,12 +81,30 @@ const Currency = () => {
                 ))}
               </select>
             </div>
+          </div>
+          <div className="flex gap-3 items-center">
+            <label htmlFor="fromAmt">Amount</label>
+            <input
+              type="text"
+              name="fromAmt"
+              value={fromAmt}
+              onChange={(e) => setFromAmt(e.target.value)}
+              placeholder="Enter the Amount to Convert"
+              className="border rounded p-3 w-full"
+            />
+          </div>
 
+          <button
+            className="bg-green-300 text-green-900 hover:bg-green-600 hover:text-white px-4 py-2 border rounded hover:shadow-md w-full"
+            onClick={Convert}
+          >
+            Convert
+          </button>
 
-            <div>
-                <label htmlFor="amt">Amount</label>
-                <input type="text" name=""/>
-            </div>
+          <div className="border" />
+
+          <div className="flex gap-3 items-center">
+            <label htmlFor="toAmt">Converted Amount : {toAmt?toAmt:"XXXXXX"}</label>
           </div>
         </div>
       </div>
