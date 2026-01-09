1edb1c8e0bee84cecb299947cc0c01f113d24ec3
GauravAthode <gauravathode123@gmail.com>
2026-01-09 23:25:51 +0530

    feat: Implement user authentication with registration, login, and logout functionality
    - Added user model with fields: fullName, email, phone, and password.
    - Created controller functions for user registration, login, and logout.
    - Set up routes for authentication in myRouter.js.
    - Integrated MongoDB connection using mongoose.
    - Updated index.js to include authentication routes and database connection.
    - Added nodemon for development convenience.

--- NODE/firstBackend/index.js
+++ NODE/firstBackend/index.js
@@ -1,8 +1,15 @@
 import dotenv from 'dotenv'
 dotenv.config();
 import express from 'express'
+import connectDB from "./src/config/db.js";
+import AuthRouter from "./src/routers/myRouter.js"
 
 const app=express();
+
+app.use(express.json());
+
+app.use("/auth",AuthRouter);
+
 
 app.get("/",(req,res)=>{
     console.log("server is Running");
@@ -12,4 +19,5 @@ app.get("/",(req,res)=>{
 const port=process.env.PORT||5000;
 app.listen(port,()=>{
     console.log("server started t port",port);
+    connectDB();
 })
