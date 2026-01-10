7fbd501b5da1066a5b29b5036d7f49daad1efb9b
GauravAthode <gauravathode123@gmail.com>
2026-01-10 22:20:49 +0530

    commit

--- NODE/firstBackend/index.js
+++ NODE/firstBackend/index.js
@@ -1,23 +1,30 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 dotenv.config();
-import express from 'express'
-import connectDB from "./src/config/db.js";
-import AuthRouter from "./src/routers/myRouter.js"
 
-const app=express();
+import express from "express";
+import connectDB from "./src/config/db.js";
+import AuthRouter from "./src/routers/myRouter.js";
+
+const app = express();
 
 app.use(express.json());
 
-app.use("/auth",AuthRouter);
+app.use("/auth", AuthRouter);
 
+app.get("/", (req, res) => {
+  console.log("Server is Running");
+  res.json({ message: "Server is Running Sucessfully" });
+});
 
-app.get("/",(req,res)=>{
-    console.log("server is Running");
-    res.json({message:"server is running sucessfully"});
-})
+app.use((err, req, res, next) => {
+  const ErrorMessage = err.message || "Internal Server Error";
+  const StatusCode = err.statusCode || 500;
 
-const port=process.env.PORT||5000;
-app.listen(port,()=>{
-    console.log("server started t port",port);
-    connectDB();
-})
+  res.status(StatusCode).json({ message: ErrorMessage });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log("Server Started at port ", port);
+  connectDB();
+});
