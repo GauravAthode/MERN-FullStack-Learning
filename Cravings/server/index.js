53c78ed0ac7bdbb2648c5251b67e26bf42114394
GauravAthode <gauravathode123@gmail.com>
2026-01-13 21:16:42 +0530

    feat: Implement user authentication with registration and login functionality

--- Cravings/server/index.js
+++ Cravings/server/index.js
@@ -1,23 +1,32 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
-import cors from 'cors';
-import connectDB from './src/config/db.js';
+import cors from "cors";
+import morgan from "morgan";
+import connectDB from "./src/config/db.js";
+import AuthRouter from "./src/routers/authRouter.js";
 
- const app= express();
+const app = express();
 
- app.use(cors({origin:"http://localhost:5173"}));
+app.use(cors({ origin: "http://localhost:5173" }));
+app.use(express.json());
+app.use(morgan("dev"));
 
- app.use(express.json());
+app.use("/auth", AuthRouter);
 
-//  app.use("/aurt",AuthRouter);
+app.get("/", (req, res) => {
+  console.log("Server is Working");
+});
 
- app.get("/",(req,res)=>{
-    console.log("Server is Working")
- })
+app.use((err, req, res, next) => {
+  const ErrorMessage = err.message || "Internal Server Error";
+  const StatusCode = err.statusCode || 500;
 
- const port = process.send.PORT || 5000;
- app.listen(port,()=>{
-    console.log("Server Started at port:", port);
-    connectDB();
- })
+  res.status(StatusCode).json({ message: ErrorMessage });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log("Server Started at Port: ", port);
+  connectDB();
+});
