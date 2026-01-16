a1a22a395901e46fe2b95c222023020f1bbd741c
GauravAthode <gauravathode123@gmail.com>
2026-01-16 21:31:48 +0530

    feat: Refactor user dashboard components and implement sidebar navigation

--- Cravings/client/src/pages/dashboards/UserDashboard.jsx
+++ Cravings/client/src/pages/dashboards/UserDashboard.jsx
@@ -1,12 +1,27 @@
-import React from "react";
-import SideBar from "../../components/userDashborad/userSideBar";
+import React, { useState } from "react";
+import UserSideBar from "../../components/userDashboard/UserSideBar";
+import UserOverview from "../../components/userDashboard/userOverview";
+import UserProfile from "../../components/userDashboard/UserProfile";
+import UserOrders from "../../components/userDashboard/UserOrders";
+import UserTransactions from "../../components/userDashboard/userTransactions";
+import UserHelpDesk from "../../components/userDashboard/UserHelpDesk";
 
 const UserDashboard = () => {
+  const [active, setActive] = useState("overview");
+
   return (
     <>
-      <div className="flex h-[90vh] w-full">
-        <div className="bg-(--color-background) border-green-500 w-2/10"><SideBar/></div>
-        <div className="border border-amber-800 w-8/10">Content</div>
+      <div className="w-full h-[90vh] flex">
+        <div className="bg-(--color-background) w-2/10">
+          <UserSideBar active={active} setActive={setActive} />
+        </div>
+        <div className="border border-amber-700 w-8/10">
+          {active === "overview" && <UserOverview />}
+          {active === "profile" && <UserProfile />}
+          {active === "orders" && <UserOrders />}
+          {active === "transactions" && <UserTransactions />}
+          {active === "helpdesk" && <UserHelpDesk />}
+        </div>
       </div>
     </>
   );
